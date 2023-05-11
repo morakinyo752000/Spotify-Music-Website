7be41868b71d8f9eb3521e5ac7d5268769f560ba
Morakinyo Akin-Taylor <79984501+morakinyo752000@users.noreply.github.com>
2023-05-11 14:06:22 +0100

    Add files via upload

---
+++ musics/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from .views import homePage, addMusic
+
+app_name='musics'
+
+urlpatterns = [
+    path('', homePage, name='home_page'),
+    path('add/', addMusic,name="add_music")
+   
+]
